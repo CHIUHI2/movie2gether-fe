56f78f204dbb28b32ba40f172c1e251a3476f83c
Tom Lo <loto2@oocl.com>
2020-12-13 19:06:15 +0800

    [setup][tom] feat: add hooks for authentication

---
+++ src/hooks/use-provide-auth.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+
+const useProvideAuth = () => {
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    setUser({ name: 'test' });
+  }, []);
+
+  const signIn = () => true;
+
+  const signOut = () => {
+    setUser({});
+  };
+
+  const isLoggedIn = () => {
+    console.log('auth check');
+    return false;
+  };
+
+  return [user, isLoggedIn, signIn, signOut];
+};
+
+export default useProvideAuth;
