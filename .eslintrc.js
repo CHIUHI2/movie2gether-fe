fe01bb88a456ded49c59466defe2e9c3f2e3b062
Tom Lo <loto2@oocl.com>
2020-12-14 12:12:02 +0800

    [setup][tom] chore: use fabric as linter config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,67 +1,7 @@
 module.exports = {
-  env: {
-    browser: true,
-    node: true,
-    jest: true,
-  },
-  extends: ['airbnb', 'airbnb/hooks'],
-  plugins: ['react', 'react-hooks', 'import', 'simple-import-sort'],
+  extends: [require.resolve('@umijs/fabric/dist/eslint')],
+
   rules: {
     'react/react-in-jsx-scope': 0,
-    'react/jsx-boolean-value': ['error', 'never', { always: [] }],
-    'react/sort-comp': [
-      'off',
-      {
-        order: [
-          'static-methods',
-          'instance-variables',
-          'lifecycle',
-          '/^on.+$/',
-          'getters',
-          'setters',
-          '/^(get|set)(?!(InitialState$|DefaultProps$|ChildContext$)).+$/',
-          'instance-methods',
-          'everything-else',
-          'rendering',
-        ],
-        groups: {
-          lifecycle: [
-            'displayName',
-            'propTypes',
-            'contextTypes',
-            'childContextTypes',
-            'mixins',
-            'statics',
-            'defaultProps',
-            'constructor',
-            'getDefaultProps',
-            'getInitialState',
-            'state',
-            'getChildContext',
-            'componentWillMount',
-            'componentDidMount',
-            'componentWillReceiveProps',
-            'shouldComponentUpdate',
-            'componentWillUpdate',
-            'componentDidUpdate',
-            'componentWillUnmount',
-          ],
-          rendering: ['/^render.+$/', 'render'],
-        },
-      },
-    ],
-    'simple-import-sort/imports': 'error',
-    'react/prop-types': 0,
-  },
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', 'ts', 'tsx', '.json'],
-      },
-    },
-    react: {
-      pragma: 'React',
-      version: 'detect',
-    },
   },
 };
