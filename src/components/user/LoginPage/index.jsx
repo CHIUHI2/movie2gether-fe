aa51eec0026fd9d289d26329c72586204288846f
Tom Lo <loto2@oocl.com>
2020-12-14 21:17:04 +0800

    [user-login-13][tom] feat: add two way link in login page

--- src/components/user/LoginPage/index.jsx
+++ src/components/user/LoginPage/index.jsx
@@ -1,14 +1,27 @@
 import './index.css';
 import { WhiteSpace } from 'antd-mobile';
+import { Link } from 'react-router-dom';
 import LoginForm from '../LoginForm';
 
-const LoginPage = () => (
-  <div>
-    <WhiteSpace />
-    <div className="user-login-title">Login</div>
-    <WhiteSpace />
-    <LoginForm />
-  </div>
-);
+const LoginPage = () => {
+  const RedirectRegistrationButton = () => (
+    <>
+      <Link to="/user/register">Register here</Link>
+    </>
+  );
+
+  return (
+    <div>
+      <WhiteSpace />
+      <div className="user-login-title">Login</div>
+      <WhiteSpace />
+      <LoginForm />
+      <WhiteSpace />
+      <span>
+        Don&apos;t have an account? <RedirectRegistrationButton />
+      </span>
+    </div>
+  );
+};
 
 export default LoginPage;
--- src/components/user/UserRegistrationPage/index.jsx
+++ src/components/user/UserRegistrationPage/index.jsx
@@ -2,15 +2,28 @@ import './index.css';
 
 import { WhiteSpace } from 'antd-mobile';
 
+import { Link } from 'react-router-dom';
 import UserRegistrationForm from '../UserRegistrationForm';
 
-const UserRegistrationPage = () => (
-  <div>
-    <WhiteSpace />
-    <div className="user-registration-title">User Registration</div>
-    <WhiteSpace />
-    <UserRegistrationForm />
-  </div>
-);
+const UserRegistrationPage = () => {
+  const RedirectLoginButton = () => (
+    <>
+      <Link to="/login">Login instead</Link>
+    </>
+  );
+
+  return (
+    <div>
+      <WhiteSpace />
+      <div className="user-registration-title">User Registration</div>
+      <WhiteSpace />
+      <UserRegistrationForm />
+      <WhiteSpace />
+      <span>
+        <RedirectLoginButton />
+      </span>
+    </div>
+  );
+};
 
 export default UserRegistrationPage;
