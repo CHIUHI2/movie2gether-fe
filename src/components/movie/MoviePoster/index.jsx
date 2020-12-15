60a36548d13459a179a5b03398d2342222cf066e
Tom Lo <loto2@oocl.com>
2020-12-15 15:56:36 +0800

    [movie-detail-18][tom] fix: MoviePoster render on received posterUrl

--- src/components/movie/MoviePoster/index.jsx
+++ src/components/movie/MoviePoster/index.jsx
@@ -4,7 +4,7 @@ import { POSTER_BASE_URL } from '../../../common/constants/movie';
 const MoviePoster = ({ posterUrl, width = 500 }) => {
   const posterPath = `${POSTER_BASE_URL}/w${width}${posterUrl}`;
 
-  return <img className="movie-detail-poster" src={posterPath} alt="Poster" />;
+  return <>{posterUrl && <img className="movie-detail-poster" src={posterPath} alt="Poster" />}</>;
 };
 
 export default MoviePoster;
