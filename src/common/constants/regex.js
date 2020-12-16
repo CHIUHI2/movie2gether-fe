716305ff18fdfbaf05906ba71c70fff96a4b77f7
Tom Lo <loto2@oocl.com>
2020-12-16 21:20:11 +0800

    [bugfix-payment][tom] fix: validate form field by validator

--- src/common/constants/regex.js
+++ src/common/constants/regex.js
@@ -2,5 +2,14 @@ const PASSWORD_VALIDATION_REGEX = new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-
 const EMAIL_VALIDATION_REGEX = new RegExp(
   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
 );
+const VISA_TYPE_MATCH_REGEX = new RegExp(/^[4][0-9]*$/);
+const MASTER_TYPE_MATCH_REGEX = new RegExp(/^[5][0-9]*$/);
+const CVC_MATCH_REGEX = new RegExp(/^[0-9]{3}$/);
 
-export { PASSWORD_VALIDATION_REGEX, EMAIL_VALIDATION_REGEX };
+export {
+  PASSWORD_VALIDATION_REGEX,
+  EMAIL_VALIDATION_REGEX,
+  VISA_TYPE_MATCH_REGEX,
+  MASTER_TYPE_MATCH_REGEX,
+  CVC_MATCH_REGEX,
+};
