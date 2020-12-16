45de208faec6b7bd7d2a7dcf5ed0f7e2640c75a5
Ray Lam <ray.lam@oocl.com>
2020-12-16 15:35:45 +0800

    [userprofile 45 rating 40][ray & david] feat: add userprofile path

--- src/router/AppRouter/index.jsx
+++ src/router/AppRouter/index.jsx
@@ -12,10 +12,11 @@ const AppRouter = () => (
   <AppLayout>
     <Router>
       <Switch>
-        <Route exact path="/login" component={UserProfilePage} />
+        <Route exact path="/login" component={LoginPage} />
         <Route exact path="/user/register" component={UserRegistrationPage} />
         <Route exact path="/test" />
         <Route exact path="/movies/:id" component={MovieDetail} />
+        <Route exact path="/userprofile/" component={UserProfilePage} />
         <Route exact path="/" component={MovieListingPage} />
         <Route path="/404" component={NotFoundPage} />
         <Route component={NotFoundPage} />
