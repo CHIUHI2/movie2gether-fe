a4831e4571393f8b52fc7ab6036306335ab1d12f
davidltk123 <davidltk123@gmail.com>
2020-12-16 10:48:30 +0800

    [userprofile 45 rating 40][ray & david] feat: connect backend API

--- src/components/review/ReviewModal/index.jsx
+++ src/components/review/ReviewModal/index.jsx
@@ -1,16 +1,50 @@
-import { Modal, WhiteSpace, WingBlank } from 'antd-mobile';
-import React from 'react';
-import StarRating from '../StarRating';
-import CommentTextArea from '../CommentTextArea';
+import { Modal, WhiteSpace, WingBlank, TextareaItem } from 'antd-mobile';
+import { StarFilled } from '@ant-design/icons';
+import './index.css';
+import React, { useState, useEffect } from 'react';
+import { getReview, addReview, updateReview } from '../../../api/review';
 
-const ReviewModal = ({openModal, closeModal, rating, setRating, comment, setComment, movieTitle}) => {
-  // const [rating, setRating] = useState(null);
-  // const [comment, setComment] = useState(null);
+const ReviewModal = ({ openModal, closeModal, movieTitle }) => {
+  const [rating, setRating] = useState(null);
+  const [comment, setComment] = useState(null);
+  const [reviewId, setReviewId] = useState(null);
+
+  useEffect(() => {
+    getReview('5fd77c99e5f7d6417d7abac2', '5fd81ac741ea7016828cfd39').then((response) => {
+      setRating(response.data.rating);
+      setComment(response.data.comment);
+      setReviewId(response.data.id);
+    });
+  }, []);
 
   const onClose = () => {
+    if (reviewId == null) {
+      const review = {
+        movieId: '5fd77c99e5f7d6417d7abac2',
+        userId: '5fd81ac741ea7016828cfd39',
+        rating,
+        comment,
+      };
+      addReview(review).then((response) => {
+        setRating(response.data.rating);
+        setComment(response.data.comment);
+        setReviewId(response.data.id);
+      });
+    } else {
+      const updatedReview = {
+        movieId: '5fd77c99e5f7d6417d7abac2',
+        userId: '5fd81ac741ea7016828cfd39',
+        rating,
+        comment,
+      };
+      updateReview(reviewId, updatedReview).then((response) => {
+        setRating(response.data.rating);
+        setComment(response.data.comment);
+      });
+    }
+
     closeModal();
-    // console.log(userId + movieId);
-};
+  };
 
   return (
     <WingBlank>
@@ -30,9 +64,29 @@ const ReviewModal = ({openModal, closeModal, rating, setRating, comment, setComm
         ]}
       >
         <WhiteSpace />
-        <CommentTextArea comment={comment} setComment={setComment}/>
+        <TextareaItem
+          className="comment"
+          rows={5}
+          count={100}
+          value={comment}
+          placeholder="Write your comment here..."
+          onChange={(content) => setComment(content)}
+        />
         <WhiteSpace />
-        <StarRating rating={rating} setRating={setRating} />
+        <div>
+          <span>Rating: </span>
+          {[...Array(5)].map((star, index) => {
+            const ratingvalue = index + 1;
+
+            return (
+              <StarFilled
+                className="star"
+                style={ratingvalue <= rating ? { color: '#ffc107' } : { color: '#e4e5e9' }}
+                onClick={() => setRating(ratingvalue)}
+              />
+            );
+          })}
+        </div>
       </Modal>
     </WingBlank>
   );
--- src/components/userprofile/UserProfilePage/index.jsx
+++ src/components/userprofile/UserProfilePage/index.jsx
@@ -1,25 +1,18 @@
-import { Flex, Button,List,Pagination} from 'antd-mobile';
-import React, { useEffect, useState} from 'react';
-import { getUser, getSessionBySessionId } from "../../../api/userprofile";
-import ReviewModal from "../../review/ReviewModal";
-import { getReview } from "../../../api/review";
-// addReview
-// updateReview
-const {Item} = List;
-const {Brief} = Item;
+import { Flex, Button, List, Pagination } from 'antd-mobile';
+import React, { useEffect, useState } from 'react';
+import { getUser, getSessionBySessionId } from '../../../api/userprofile';
+import ReviewModal from '../../review/ReviewModal';
+
+const { Item } = List;
+const { Brief } = Item;
 const UserProfilePage = () => {
   const [user, setUser] = useState({});
-  const [sessions,setSessions] = useState([]);
+  const [sessions, setSessions] = useState([]);
   const [openModal, setOpenModal] = useState(false);
-  const [rating, setRating] = useState(null);
-  const [comment, setComment] = useState(null);
 
-  const showModal = () => (
-    getReview('5fd77c99e5f7d6417d7abac4', '5fd81ac741ea7016828cfd40').then((response) => {
-      setRating(response.data.rating);
-      setComment(response.data.comment);
-    }).finally(()=>{setOpenModal(true)}))
-  ;
+  const showModal = () => {
+    setOpenModal(true);
+  };
 
   const onClose = () => {
     setOpenModal(false);
@@ -33,59 +26,60 @@ const UserProfilePage = () => {
   useEffect(() => {
     getUser('1').then((response) => {
       setUser(response.data);
-    })
-  }, [])
+    });
+  }, []);
 
   useEffect(() => {
-    if(user.id){
-     getSessionBySessionId(user.id).then((response) => {
-          setSessions(response.data)
-        })
-      }
-  }, [user.id])
+    if (user.id) {
+      getSessionBySessionId(user.id).then((response) => {
+        setSessions(response.data);
+      });
+    }
+  }, [user.id]);
 
   const GenerateListItem = () => {
     return (
       <div>
-      <ReviewModal openModal={openModal} closeModal={onClose}
-      rating={rating}
-      setRating={setRating}
-      comment={comment}
-      setComment={setComment}
-      movieTitle='Mr Bean'/>
-          {
-              sessions.map((session) =>
-              <Item key={session.id} multipleLine arrow="horizontal" onClick={showModal} >
-              {session.movieName} <Brief> {session.date}</Brief>
-            </Item>
-              )
-          }
+        <ReviewModal
+          openModal={openModal}
+          closeModal={onClose}
+          movieTitle="Mr Bean"
+        />
+        {sessions.map((session) => (
+          <Item key={session.id} multipleLine arrow="horizontal" onClick={showModal}>
+            {session.movieName} <Brief> {session.date}</Brief>
+          </Item>
+        ))}
       </div>
-  );
-  }
-
-  const PaginationItem = () => {
-    return (
-      <Pagination total={5} current={1} locale={locale} />
-    );
-  }
-    return (
-      <>
-       <div>
-       <Flex justify="center">User Profile</Flex>
-       <List renderHeader={() => 'User Information'} className="my-list">
-        <Item>User Name: <span>{user.userName}</span></Item>
-        <Item>Email: <span>{user.email}</span></Item>
-        <Item arrow="horizontal" multipleLine onClick={() => {}}>My Friend List </Item>
-      </List>
-      <List renderHeader={() => 'Movie History'} className="my-list">
-        <GenerateListItem />
-      </List>
-       </div>
-        <PaginationItem />
-       <Button>Back</Button>
-      </>
     );
   };
-  // todo pagination to constant
-  export default UserProfilePage;
+
+  const PaginationItem = () => {
+    return <Pagination total={5} current={1} locale={locale} />;
+  };
+  return (
+    <>
+      <div>
+        <Flex justify="center">User Profile</Flex>
+        <List renderHeader={() => 'User Information'} className="my-list">
+          <Item>
+            User Name: <span>{user.userName}</span>
+          </Item>
+          <Item>
+            Email: <span>{user.email}</span>
+          </Item>
+          <Item arrow="horizontal" multipleLine onClick={() => {}}>
+            My Friend List{' '}
+          </Item>
+        </List>
+        <List renderHeader={() => 'Movie History'} className="my-list">
+          <GenerateListItem />
+        </List>
+      </div>
+      <PaginationItem />
+      <Button>Back</Button>
+    </>
+  );
+};
+// todo pagination to constant
+export default UserProfilePage;
