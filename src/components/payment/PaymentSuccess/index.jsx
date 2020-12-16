0dd1bf40de7023f0b387085f75f1d1e345db307b
Tom Lo <loto2@oocl.com>
2020-12-16 21:39:08 +0800

    [bugfix-payment][tom] fix: remove purchase again btn for successful

--- src/components/payment/PaymentSuccess/index.jsx
+++ src/components/payment/PaymentSuccess/index.jsx
@@ -7,18 +7,9 @@ const PaymentSuccess = () => {
   const location = useLocation();
   const history = useHistory();
   const orderId = _.get(location, 'state.orderId', '');
-  const sessionId = _.get(location, 'state.sessionId', '');
-  const seatNumber = _.get(location, 'state.seatNumber', '');
 
   const handleBackToHome = () => {
     history.push('/');
-  };
-
-  const handlePurchaseAgain = () => {
-    history.push('/payment', {
-      sessionId,
-      seatNumber,
-    });
   };
 
   return (
@@ -28,9 +19,6 @@ const PaymentSuccess = () => {
         <p id="order">Order number:{orderId} </p>
         <Button className="button" type="primary" key="console" onClick={handleBackToHome}>
           Back to Home
-        </Button>
-        <Button className="button" type="primary" key="buy" onClick={handlePurchaseAgain}>
-          Purchase Again
         </Button>
       </div>
     </>
