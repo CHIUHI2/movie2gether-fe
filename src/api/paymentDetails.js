cc27d9d6606161dcf762b4c5a89daa1dbfb182c7
Tom Lo <loto2@oocl.com>
2020-12-16 14:05:49 +0800

    [payment][tom] feat: update payment api path

--- src/api/paymentDetails.js
+++ src/api/paymentDetails.js
@@ -3,14 +3,14 @@ import api from './api';
 const updatePaidStatus = (booking) => api.put(`/booking/${booking.id}`, booking);
 const getUser = (id) => {
   return api.get(`/user/${id}`);
- };
- const getBookingById = (id) => {
+};
+const getBookingById = (id) => {
   return api.get(`/booking?userId=${id}`);
-}
+};
 const getSessionById = (id) => {
-  return api.get(`/session/?userId=${id}`);
-}
+  return api.get(`/sessions/${id}`);
+};
 const getMovieById = (id) => {
   return api.get(`/booking?sessionId=${id}`);
-}
-export {getUser, getBookingById, getSessionById, getMovieById,updatePaidStatus};
+};
+export { getUser, getBookingById, getSessionById, getMovieById, updatePaidStatus };
