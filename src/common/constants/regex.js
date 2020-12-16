861d4ae2170727634f4c8bf73ebd58493d162d09
Hins Chiu <chiuhi2@oocl.com>
2020-12-16 19:15:27 +0800

    [User][Hins]fix:allow special character for password on Login and Registration

--- src/common/constants/regex.js
+++ src/common/constants/regex.js
@@ -1,4 +1,4 @@
-const PASSWORD_VALIDATION_REGEX = new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/);
+const PASSWORD_VALIDATION_REGEX = new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z.!#$%&'*+/=?^_`{|}~-]{8,}$/);
 const EMAIL_VALIDATION_REGEX = new RegExp(
   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
 );
