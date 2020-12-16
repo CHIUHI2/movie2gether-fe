4ccaa4ca6d542f6d4db227a5dc685faffb7879f2
Tom Lo <loto2@oocl.com>
2020-12-16 09:38:54 +0800

    [movie-listing-17][tom] feat: homepage use movie listing

--- src/router/AppRouter/index.jsx
+++ src/router/AppRouter/index.jsx
@@ -1,6 +1,4 @@
-import {
-  BrowserRouter as Router, Route, Switch,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import NotFoundPage from '../../components/exception/NotFoundPage';
 import MovieListingPage from '../../components/movie/MovieListingPage';
@@ -8,18 +6,16 @@ import MovieDetail from '../../components/movie/MovieDetail';
 import LoginPage from '../../components/user/LoginPage';
 import UserRegistrationPage from '../../components/user/UserRegistrationPage';
 import AppLayout from '../../layouts/AppLayout';
-import ProtectedRoute from '../ProtectedRoute';
 
 const AppRouter = () => (
   <AppLayout>
     <Router>
       <Switch>
         <Route exact path="/login" component={LoginPage} />
-        <Route exact path="/movies" component={MovieListingPage} />
         <Route exact path="/user/register" component={UserRegistrationPage} />
         <Route exact path="/test" />
         <Route exact path="/movies/:id" component={MovieDetail} />
-        <ProtectedRoute exact path="/" />
+        <Route exact path="/" component={MovieListingPage} />
         <Route path="/404" component={NotFoundPage} />
         <Route component={NotFoundPage} />
       </Switch>
